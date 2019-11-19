95b8809865d50c695330805b70a2a47ef9d162eb
joseluiscj <jesecruzcj@gmail.com>
2019-11-19 14:01:32 -0500

    matricula03

--- app/Http/Controllers/AplicacionController.php
+++ app/Http/Controllers/AplicacionController.php
@@ -26,11 +26,14 @@ class AplicacionController extends Controller
     public function DatosMatricula(Request $dato)
     {
         $_auxdni = $dato->input('txtDni');
-        $datos = DB::select("call buscarAlumnodni(?)", array($_auxdni));
-        //return $datos;
-        return view('Mantenedor.MatriculaFrmRegistro', ['datos' => $datos]);
-
-        //return   view('Mantenedor.MatriculaFrm',compact(datos));
+        $_numcade = strlen ($_auxdni);
+        if ($_numcade = 8) {
+            $datos = DB::select("call buscarAlumnodni(?)", array($_auxdni));
+            return view('Mantenedor.MatriculaFrm', ['datos' => $datos]);
+        } else {
+            return view('Mantenedor.MatriculaFrm');
+            //return view('Mantenedor.MatriculaFrm', ['_auxdni' => $_auxdni]);
+        }
     }
     //********************FIN_Matricula********************* */
     public function Ingresos(REQUEST $request)
