aa626319e5bb219eb7ac7c91bdbd7779fe300886
GabrielaRosmeri <51028447+GabrielaRosmeri@users.noreply.github.com>
2019-11-25 21:58:49 -0500

    base

--- bd_megasystem.sql
+++ bd_megasystem.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100134
  File Encoding         : 65001
 
- Date: 20/11/2019 21:58:52
+ Date: 25/11/2019 21:47:16
 */
 
 SET NAMES utf8mb4;
@@ -32,7 +32,7 @@ CREATE TABLE `alumno`  (
   `feNacimiento` date NULL DEFAULT NULL,
   `direccion` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   PRIMARY KEY (`idAlumno`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 19 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 25 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of alumno
@@ -40,6 +40,10 @@ CREATE TABLE `alumno`  (
 INSERT INTO `alumno` VALUES (16, '70409311', 'Jose Luis', 'Cruz', 'Julcarima', b'0', '948795936', '1997-05-02', 'calle Arica#1247');
 INSERT INTO `alumno` VALUES (17, '70409359', 'Chrintian', 'Carrasco', 'García', b'0', '948759784', '1995-06-15', 'Piura');
 INSERT INTO `alumno` VALUES (18, '74809025', 'Patricia Daniela', 'Mil', 'Limo', b'0', '847958629', '1997-11-14', 'Chiclayo-09 de Octubre');
+INSERT INTO `alumno` VALUES (19, '12345678', 'Gómez', 'Jese', 'Mongragón', b'1', '124569878', '2019-11-18', 'sadfsdfe we fw');
+INSERT INTO `alumno` VALUES (22, '12345678', 'aaaaaaaa', 'aaaaaaaaaa', 'aaaaaaaaaaa', b'1', '234234234', '2019-11-25', 'asdasd');
+INSERT INTO `alumno` VALUES (23, '12345678', 'aaaaaaaa', 'aaaaaaaaaa', 'aaaaaaaaaaa', b'1', '234234234', '2019-11-25', 'asdasd');
+INSERT INTO `alumno` VALUES (24, '70409311', 'Jose Luis', 'Cruz', 'Julcarima', b'1', '948795936', '1997-05-02', 'calle Arica#1247');
 
 -- ----------------------------
 -- Table structure for apoderado
@@ -53,9 +57,16 @@ CREATE TABLE `apoderado`  (
   `apMaterno` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   `celular` char(9) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   `direccion` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `parentesco` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `parentesco` char(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   PRIMARY KEY (`idApoderado`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of apoderado
+-- ----------------------------
+INSERT INTO `apoderado` VALUES (1, '87654321', 'bbbbbb', 'bbbbbbbb', 'bbbbbbbb', '234', 'sdfsdf', 'sfsd');
+INSERT INTO `apoderado` VALUES (2, '87654321', 'bbbbbb', 'bbbbbbbb', 'bbbbbbbb', '234', 'sdfsdf', 'sfsd');
+INSERT INTO `apoderado` VALUES (3, '11111111', 'qqqq', 'qqqqq', 'qqqq', '145662', 'sadfsdf', 'tio');
 
 -- ----------------------------
 -- Table structure for ciclo
@@ -63,17 +74,17 @@ CREATE TABLE `apoderado`  (
 DROP TABLE IF EXISTS `ciclo`;
 CREATE TABLE `ciclo`  (
   `idCiclo` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(25) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `nombre` char(15) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   `feInicio` date NULL DEFAULT NULL,
   `feFin` date NULL DEFAULT NULL,
-  `activo` bit(1) NULL DEFAULT NULL,
+  `activo` bit(1) NULL DEFAULT NULL COMMENT '1-> Activo ',
   PRIMARY KEY (`idCiclo`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of ciclo
 -- ----------------------------
-INSERT INTO `ciclo` VALUES (1, '2019-3', '2019-09-02', '2019-12-20', b'1');
+INSERT INTO `ciclo` VALUES (2, '2020_I', '2020-01-06', '2020-04-30', b'1');
 
 -- ----------------------------
 -- Table structure for concepto
@@ -82,30 +93,14 @@ DROP TABLE IF EXISTS `concepto`;
 CREATE TABLE `concepto`  (
   `idConcepto` int(11) NOT NULL AUTO_INCREMENT,
   `descripcion` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `moTotal` float(255, 2) NULL DEFAULT NULL,
   PRIMARY KEY (`idConcepto`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of concepto
 -- ----------------------------
-INSERT INTO `concepto` VALUES (1, 'Certificado');
-
--- ----------------------------
--- Table structure for controlpago_cuota
--- ----------------------------
-DROP TABLE IF EXISTS `controlpago_cuota`;
-CREATE TABLE `controlpago_cuota`  (
-  `idControlPagoCuota` int(255) NOT NULL AUTO_INCREMENT,
-  `importe` float(255, 2) NULL DEFAULT NULL,
-  `saldo` float(255, 2) NULL DEFAULT NULL,
-  `idPago` int(11) NULL DEFAULT NULL,
-  `idCuota` int(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`idControlPagoCuota`) USING BTREE,
-  INDEX `idCuota`(`idCuota`) USING BTREE,
-  INDEX `idPago`(`idPago`) USING BTREE,
-  CONSTRAINT `controlpago_cuota_ibfk_1` FOREIGN KEY (`idCuota`) REFERENCES `cuota` (`idCuota`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `controlpago_cuota_ibfk_2` FOREIGN KEY (`idPago`) REFERENCES `pago` (`idPago`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+INSERT INTO `concepto` VALUES (1, 'Certificado', NULL);
 
 -- ----------------------------
 -- Table structure for cuota
@@ -113,14 +108,14 @@ CREATE TABLE `controlpago_cuota`  (
 DROP TABLE IF EXISTS `cuota`;
 CREATE TABLE `cuota`  (
   `idCuota` int(11) NOT NULL AUTO_INCREMENT,
-  `monto` float(255, 2) NULL DEFAULT NULL,
-  `feVencimiento` datetime(0) NULL DEFAULT NULL,
-  `pagado` float(255, 0) NULL DEFAULT NULL,
-  `descripcion` char(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `importe` float(255, 2) NULL DEFAULT NULL,
+  `feEmision` datetime(0) NULL DEFAULT NULL,
   `idMatricula` int(11) NULL DEFAULT NULL,
+  `idPago` int(11) NULL DEFAULT NULL,
   PRIMARY KEY (`idCuota`) USING BTREE,
   INDEX `idMatricula`(`idMatricula`) USING BTREE,
-  CONSTRAINT `cuota_ibfk_1` FOREIGN KEY (`idMatricula`) REFERENCES `matricula` (`idMatricula`) ON DELETE RESTRICT ON UPDATE RESTRICT
+  INDEX `idPago`(`idPago`) USING BTREE,
+  CONSTRAINT `cuota_ibfk_1` FOREIGN KEY (`idPago`) REFERENCES `pago` (`idPago`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
@@ -130,38 +125,45 @@ DROP TABLE IF EXISTS `curso`;
 CREATE TABLE `curso`  (
   `idCurso` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `idCiclo` int(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`idCurso`) USING BTREE,
-  INDEX `idCiclo`(`idCiclo`) USING BTREE,
-  CONSTRAINT `curso_ibfk_1` FOREIGN KEY (`idCiclo`) REFERENCES `ciclo` (`idCiclo`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+  PRIMARY KEY (`idCurso`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of curso
 -- ----------------------------
-INSERT INTO `curso` VALUES (2, 'Excel Avanzado', 1);
-INSERT INTO `curso` VALUES (3, 'Word', 1);
-INSERT INTO `curso` VALUES (4, 'PowerPoint', 1);
+INSERT INTO `curso` VALUES (5, 'Word');
+INSERT INTO `curso` VALUES (6, 'Excel');
+INSERT INTO `curso` VALUES (7, 'Power Point');
+INSERT INTO `curso` VALUES (8, 'Computación');
 
 -- ----------------------------
 -- Table structure for grupo
 -- ----------------------------
 DROP TABLE IF EXISTS `grupo`;
 CREATE TABLE `grupo`  (
-  `idGrupo` int(255) NOT NULL AUTO_INCREMENT,
+  `idGrupo` int(11) NOT NULL AUTO_INCREMENT,
   `descripcion` varchar(30) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   `idCurso` int(11) NULL DEFAULT NULL,
-  `duracion` int(2) NULL DEFAULT NULL,
+  `duMeses` int(2) NULL DEFAULT NULL,
+  `duTotalHoras` int(3) NULL DEFAULT NULL,
+  `idCiclo` int(11) NULL DEFAULT NULL,
+  `vacante` int(11) NULL DEFAULT NULL,
+  `hoInicio` int(2) NULL DEFAULT NULL,
+  `hoFin` int(2) NULL DEFAULT NULL,
+  `dia` char(9) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   PRIMARY KEY (`idGrupo`) USING BTREE,
   INDEX `idCurso`(`idCurso`) USING BTREE,
-  CONSTRAINT `grupo_ibfk_1` FOREIGN KEY (`idCurso`) REFERENCES `curso` (`idCurso`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+  INDEX `idCiclo`(`idCiclo`) USING BTREE,
+  CONSTRAINT `grupo_ibfk_1` FOREIGN KEY (`idCurso`) REFERENCES `curso` (`idCurso`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `grupo_ibfk_2` FOREIGN KEY (`idCiclo`) REFERENCES `ciclo` (`idCiclo`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of grupo
 -- ----------------------------
-INSERT INTO `grupo` VALUES (1, 'Excel Grupo A', 2, NULL);
-INSERT INTO `grupo` VALUES (2, 'PowerPoint A', 4, NULL);
+INSERT INTO `grupo` VALUES (3, 'A (9:00am - 1:00pm)', 5, 4, 64, 2, 5, 9, 1, 'Sábado');
+INSERT INTO `grupo` VALUES (4, 'B (2:00pm - 6:00pm)', 5, 4, 64, 2, 4, 2, 6, 'Sábado');
+INSERT INTO `grupo` VALUES (5, 'A (9:00am - 1:00pm)', 6, 3, 48, 2, 4, 9, 1, 'Sábado');
 
 -- ----------------------------
 -- Table structure for matricula
@@ -169,15 +171,12 @@ INSERT INTO `grupo` VALUES (2, 'PowerPoint A', 4, NULL);
 DROP TABLE IF EXISTS `matricula`;
 CREATE TABLE `matricula`  (
   `idMatricula` int(11) NOT NULL AUTO_INCREMENT,
-  `fecha` date NULL DEFAULT NULL,
-  `observacion` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `fecha` datetime(0) NULL DEFAULT NULL,
   `idGrupo` int(11) NULL DEFAULT NULL,
   `idApoderado` int(11) NULL DEFAULT NULL,
   `idAlumno` int(11) NULL DEFAULT NULL,
   `idPromotor` int(11) NULL DEFAULT NULL,
-  `idPago` int(11) NULL DEFAULT NULL,
   PRIMARY KEY (`idMatricula`) USING BTREE,
-  INDEX `idPago`(`idPago`) USING BTREE,
   INDEX `idPromotor`(`idPromotor`) USING BTREE,
   INDEX `idAlumno`(`idAlumno`) USING BTREE,
   INDEX `idApoderado`(`idApoderado`) USING BTREE,
@@ -185,26 +184,9 @@ CREATE TABLE `matricula`  (
   CONSTRAINT `matricula_ibfk_1` FOREIGN KEY (`idApoderado`) REFERENCES `apoderado` (`idApoderado`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `matricula_ibfk_2` FOREIGN KEY (`idPromotor`) REFERENCES `promotor` (`idPromotor`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `matricula_ibfk_3` FOREIGN KEY (`idAlumno`) REFERENCES `alumno` (`idAlumno`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `matricula_ibfk_4` FOREIGN KEY (`idPago`) REFERENCES `pago` (`idPago`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `matricula_ibfk_5` FOREIGN KEY (`idGrupo`) REFERENCES `grupo` (`idGrupo`) ON DELETE RESTRICT ON UPDATE RESTRICT
+  CONSTRAINT `matricula_ibfk_5` FOREIGN KEY (`idGrupo`) REFERENCES `grupo` (`idGrupo`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `matricula_ibfk_6` FOREIGN KEY (`idMatricula`) REFERENCES `pago` (`idMatricula`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
-
--- ----------------------------
--- Table structure for migrations
--- ----------------------------
-DROP TABLE IF EXISTS `migrations`;
-CREATE TABLE `migrations`  (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `migration` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `batch` int(11) NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Compact;
-
--- ----------------------------
--- Records of migrations
--- ----------------------------
-INSERT INTO `migrations` VALUES (1, '2014_10_12_000000_create_users_table', 1);
-INSERT INTO `migrations` VALUES (2, '2014_10_12_100000_create_password_resets_table', 1);
 
 -- ----------------------------
 -- Table structure for pago
@@ -213,24 +195,18 @@ DROP TABLE IF EXISTS `pago`;
 CREATE TABLE `pago`  (
   `idPago` int(11) NOT NULL AUTO_INCREMENT,
   `monto` float(255, 2) NULL DEFAULT NULL,
-  `fecha` date NULL DEFAULT NULL,
-  `nuRecibo` varchar(15) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `feVencimiento` date NULL DEFAULT NULL,
   `idConcepto` int(11) NULL DEFAULT NULL,
+  `idMatricula` int(11) NULL DEFAULT NULL,
+  `saldo` float(255, 2) NULL DEFAULT NULL,
+  `pagado` bit(1) NULL DEFAULT NULL,
+  `raDescuento` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL,
+  `moDescuento` float(255, 2) NULL DEFAULT NULL,
   PRIMARY KEY (`idPago`) USING BTREE,
   INDEX `idConcepto`(`idConcepto`) USING BTREE,
+  INDEX `idMatricula`(`idMatricula`) USING BTREE,
   CONSTRAINT `pago_ibfk_1` FOREIGN KEY (`idConcepto`) REFERENCES `concepto` (`idConcepto`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
-
--- ----------------------------
--- Table structure for password_resets
--- ----------------------------
-DROP TABLE IF EXISTS `password_resets`;
-CREATE TABLE `password_resets`  (
-  `email` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp(0) NULL DEFAULT NULL,
-  INDEX `password_resets_email_index`(`email`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for promotor
@@ -241,51 +217,37 @@ CREATE TABLE `promotor`  (
   `nombre` varchar(30) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   `apPaterno` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   `apMaterno` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `dni` char(8) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   PRIMARY KEY (`idPromotor`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
-
--- ----------------------------
--- Table structure for users
--- ----------------------------
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users`  (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `remember_token` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  `created_at` timestamp(0) NULL DEFAULT NULL,
-  `updated_at` timestamp(0) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `users_email_unique`(`email`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Procedure structure for buscarAlumnodni
 -- ----------------------------
 DROP PROCEDURE IF EXISTS `buscarAlumnodni`;
 delimiter ;;
-CREATE PROCEDURE `buscarAlumnodni`(IN `_dni` VARCHAR(20))
+CREATE PROCEDURE `buscarAlumnodni`(IN `_auxDni` VARCHAR(10))
 BEGIN
-		#if (LENGTH(dni) = 8) THEN
-			SELECT * FROM alumno WHERE alumno.dni = _dni;
-		#end if;
+	IF (LENGTH(_auxDni) = 8) THEN
+		SELECT * FROM alumno WHERE alumno.dni = _auxDni;
+	END IF;
 END
 ;;
 delimiter ;
 
 -- ----------------------------
--- Procedure structure for buscarApoderadoId
+-- Function structure for nuevo
 -- ----------------------------
-DROP PROCEDURE IF EXISTS `buscarApoderadoId`;
+DROP FUNCTION IF EXISTS `nuevo`;
 delimiter ;;
-CREATE PROCEDURE `buscarApoderadoId`(IN `_auxId` INTEGER)
+CREATE FUNCTION `nuevo`(num INTEGER)
+ RETURNS int(11)
 BEGIN
+declare 
+	total,cont int default 1;
 
-	IF (_auxId > 0) THEN 
-		SELECT * from apoderado WHERE idApoderado = _auxId;
-	END IF;
 
+RETURN cade;
 END
 ;;
 delimiter ;
@@ -305,78 +267,23 @@ IN `_feNacimiento` date,
 IN `_direccion` VARCHAR(50))
 BEGIN
 	
-		INSERT INTO alumno VALUES(_dni,_nombre,_apPaterno,_apMaterno,_genero,_celular,_feNacimiento,_direccion);
+		INSERT INTO alumno(dni,nombre,apPaterno,apMaterno,genero,celular,feNacimiento,direccion) VALUES(_dni,_nombre,_apPaterno,_apMaterno,_genero,_celular,_feNacimiento,_direccion);
 
 END
 ;;
 delimiter ;
 
 -- ----------------------------
--- Procedure structure for registrarCiclo
+-- Procedure structure for registrarApoderado
 -- ----------------------------
-DROP PROCEDURE IF EXISTS `registrarCiclo`;
+DROP PROCEDURE IF EXISTS `registrarApoderado`;
 delimiter ;;
-CREATE PROCEDURE `registrarCiclo`(IN `nombre` VARCHAR(25),IN `feInicio` date,IN `feFin` date,IN `mes` INT(11), IN `activo` INT(225))
+CREATE PROCEDURE `registrarApoderado`(IN `_dni` VARCHAR(18),IN `_nombre` VARCHAR(30),IN `_apPaterno` VARCHAR(20), IN `_apMaterno` VARCHAR(20), IN `_celular` VARCHAR(10), IN `_direccion` VARCHAR(50),IN `_parentesco` VARCHAR(20))
 BEGIN
 
-	if mes <> 0 then 
-			IF (nombre <> '' and feInicio <> '' and feFin <> '' and mes <> '' and activo )  THEN
-				INSERT INTO ciclo(nombre,feInicio,feFin,mes,activo) VALUES(nombre, feInicio,feFin,mes,activo);
-			END IF;
-	end if;
 
-END
-;;
-delimiter ;
+			INSERT INTO apoderado(dni,nombre,apPaterno,apMaterno,celular,direccion,parentesco) VALUES(_dni,_nombre,_apPaterno,_apMaterno,_celular,_direccion,_parentesco);
 
--- ----------------------------
--- Procedure structure for registrarConcepto
--- ----------------------------
-DROP PROCEDURE IF EXISTS `registrarConcepto`;
-delimiter ;;
-CREATE PROCEDURE `registrarConcepto`(IN `nombre` VARCHAR(20))
-BEGIN
-
-	if nombre <> '' then
-		INSERT INTO concepto(descripcion) VALUES(nombre);
-	end if;
-END
-;;
-delimiter ;
-
--- ----------------------------
--- Procedure structure for RegistrarCurso
--- ----------------------------
-DROP PROCEDURE IF EXISTS `RegistrarCurso`;
-delimiter ;;
-CREATE PROCEDURE `RegistrarCurso`(IN `nombre` VARCHAR(20),IN `idCiclo` int(11))
-BEGIN
-
-	IF (nombre <> '') THEN
-	
-		INSERT INTO curso(nombre, idCiclo) VALUES(nombre,idCiclo);
-		
-	END IF;
-
-
-
-END
-;;
-delimiter ;
-
--- ----------------------------
--- Procedure structure for registroApoderado
--- ----------------------------
-DROP PROCEDURE IF EXISTS `registroApoderado`;
-delimiter ;;
-CREATE PROCEDURE `registroApoderado`(IN `dni` VARCHAR(18),IN `nombre` VARCHAR(30),IN `apPaterno` VARCHAR(20), IN `apMaterno` VARCHAR(20), IN `genero` CHAR(11), IN `celular` VARCHAR(10),IN `feNacimiento` DATE, IN `direccion` VARCHAR(50),IN `parentesco` VARCHAR(20))
-BEGIN
-
-	if (LENGTH(dni) = 8) THEN
-		IF (nombre <> '' and apPaterno <> '' and apMaterno <> '' and genero <> '' and feNacimiento <> '' and direccion <> '' 				and   		parentesco <> '')  THEN
-			INSERT INTO apoderado VALUES(dni, nombre,apPaterno,apMaterno,genero,celular,celular,feNacimiento,direccion,parentesco);
-		END IF;
-	end if;
 END
 ;;
 delimiter ;
