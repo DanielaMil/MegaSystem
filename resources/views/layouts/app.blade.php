5955f085962882dcc3da8eb3c35422eb40a92804
Ruth Esther Ramirez Requejo <r981703597@gmail.com>
2019-11-27 18:25:00 -0500

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,5 @@
 <!doctype html>
 <html lang="es">
-
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,16 +7,15 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>MEGASYSTEM</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
-    <meta name="description" content="This is an example dashboard created using build-in elements and components.">
     <meta name="msapplication-tap-highlight" content="no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <link href="{{asset('template/architectui-html-free/main.css')}}" rel="stylesheet">
 </head>
 
+
 <body>
     <div class="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header">
-        <div class="app-header header-shadow bg-primary header-text-light">
+        <div class="app-header header-shadow">
             <div class="app-header__logo">
                 <div class="logo-src"></div>
                 <div class="header__pane ml-auto">
@@ -47,18 +45,27 @@
                         </span>
                     </button>
                 </span>
+            </div>    <div class="app-header__content">
+                <!--Todo lo que va al lado del menu amborguesa-->
             </div>
-            <div class="app-header__content">
-               
-            </div>
-        </div>
-        <div class="app-main">
-            <div class="app-sidebar sidebar-shadow bg-vicious-stance sidebar-text-light">
-                <div class="app-header__logo">
-                    <div class="logo-src"></div>
-                    <div class="header__pane ml-auto">
+        </div>        
+               <div class="app-main">
+                <div class="app-sidebar sidebar-shadow">
+                    <div class="app-header__logo">
+                        <div class="logo-src"></div>
+                        <div class="header__pane ml-auto">
+                            <div>
+                                <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
+                                    <span class="hamburger-box">
+                                        <span class="hamburger-inner"></span>
+                                    </span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="app-header__mobile-menu">
                         <div>
-                            <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
+                            <button type="button" class="hamburger hamburger--elastic mobile-toggle-nav">
                                 <span class="hamburger-box">
                                     <span class="hamburger-inner"></span>
                                 </span>
@@ -108,56 +115,134 @@
                             </ul>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div class="app-main__outer">
-                <div class="app-main__inner">
-                    @yield('content')
-                </div>
-                <div class="app-wrapper-footer">
-                    <div class="app-footer">
-                        <div class="app-footer__inner">
-                            <div class="app-footer-left">
-                                <ul class="nav">
-                                    <li class="nav-item">
-                                        <a href="javascript:void(0);" class="nav-link">
-                                            Footer Link 1
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="javascript:void(0);" class="nav-link">
-                                            Footer Link 2
-                                        </a>
-                                    </li>
-                                </ul>
+                </div>    <div class="app-main__outer">
+                    <div class="app-main__inner">
+                        
+                        <!--Aqui va todo lo que está dentro -->
+                        @yield('content')
+                    </div>
+                    <div class="app-wrapper-footer">
+                        <div class="app-footer">
+                            <div class="app-footer__inner">
+                                <div class="app-footer-left">
+                                    <ul class="nav">
+                                        <li class="nav-item">
+                                            <a href="javascript:void(0);" class="nav-link">
+                                                Footer Link 1
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="javascript:void(0);" class="nav-link">
+                                                Footer Link 2
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="app-footer-right">
+                                    <ul class="nav">
+                                        <li class="nav-item">
+                                            <a href="javascript:void(0);" class="nav-link">
+                                                Footer Link 3
+                                            </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="javascript:void(0);" class="nav-link">
+                                                <div class="badge badge-success mr-1 ml-0">
+                                                    <small>NEW</small>
+                                                </div>
+                                                Footer Link 4
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
-                            <div class="app-footer-right">
-                                <ul class="nav">
-                                    <li class="nav-item">
-                                        <a href="javascript:void(0);" class="nav-link">
-                                            Footer Link 3
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="javascript:void(0);" class="nav-link">
-                                            <div class="badge badge-success mr-1 ml-0">
-                                                <small>NEW</small>
-                                            </div>
-                                            Footer Link 4
-                                        </a>
-                                    </li>
-                                </ul>
+                        </div>
+                    </div>    </div>
+                <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+        </div>
+    </div>
+<script type="text/javascript" src="./assets/scripts/main.js"></script></body>
+    <script type="text/javascript" src="{{asset('template/architectui-html-free//assets/scripts/main.js')}}"></script>
+    <script src="{{asset('js/app.js')}}"></script>
+</html>
+<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Registrar Nueno Ingreso</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form class="">
+                    <div class="row m-3">
+                        <div class="column m-3 w-50">
+                            <div class="position-relative form-group">
+                                <label for="form-control" class="">
+                                    <font style="vertical-align: inherit;">
+                                        <font style="vertical-align: inherit;">Persona</font>
+                                    </font>
+                                </label><input name="text" id="txtPaterno" type="text" class="form-control">
+                            </div>
+                            <div class="position-relative form-group">
+                                <label for="exampleSelect" class="">
+                                    <font style="vertical-align: inherit;">
+                                        <font style="vertical-align: inherit;">Concepto</font>
+                                    </font>
+                                </label>
+                                <select name="select" id="cboConcepto" class="form-control">
+                                    <option>
+
+                                    </option>
+                                </select>
+                            </div>
+                            <div class="position-relative form-group">
+                                <label for="examplePassword" class="">
+                                    <font style="vertical-align: inherit;">
+                                        <font style="vertical-align: inherit;">Fecha </font>
+                                    </font>
+                                </label><input name="text" id="txtFecha" type="date" class="form-control">
+                            </div>
+                        </div>
+                        <div class="column m-3">
+                            <div class="position-relative form-group">
+                                <label for="form-control" class="">
+                                    <font style="vertical-align: inherit;">
+                                        <font style="vertical-align: inherit;">N° de Recibo</font>
+                                    </font>
+                                </label><input name="text" id="txtNuroRecibo" type="text" class="form-control">
+                            </div>
+                            <div class="position-relative form-group">
+                                <label for="examplePassword" class="">
+                                    <font style="vertical-align: inherit;">
+                                        <font style="vertical-align: inherit;">Monto</font>
+                                    </font>
+                                </label><input name="text" id="txtMonto" type="text" class="form-control">
+                            </div>
+                            <div class="position-relative form-group"><label for="exampleText" class="">
+                                    <font style="vertical-align: inherit;">
+                                        <font style="vertical-align: inherit;">Observacion</font>
+                                    </font>
+                                </label><textarea name="text" id="txtObservacion" class="form-control"></textarea>
                             </div>
                         </div>
                     </div>
-                </div>
+                   
+                </form>
             </div>
-            <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">
+                    <font style="vertical-align: inherit;">
+                            <font style="vertical-align: inherit;">Cancelar</font>
+                    </font>
+                </button>
+                <button type="button" class="btn btn-primary">
+                    <font style="vertical-align-center: inherit;">
+                        <font style="vertical-align center: inherit;">Registrar</font>
+                    </font>
+                </button>
+            </div>
         </div>
     </div>
-    <script type="text/javascript" src="{{asset('template/architectui-html-free//assets/scripts/main.js')}}"></script>
-    <script src="{{asset('js/app.js')}}"></script>
-     @yield('js')
-</body>
-
-</html>
+</div>
