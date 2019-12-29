fa146fce7161fc8a59718388b2562eddad119cb4
joseluiscj <jesecruzcj@gmail.com>
2019-12-29 14:21:01 -0500

    ghhh

--- resources/views/Mantenedor/Matricula.blade.php
+++ resources/views/Mantenedor/Matricula.blade.php
@@ -2300,6 +2300,20 @@ PATRICIA
 
             
         }
+        /*$(document).ready(function() {
+    $('#example').DataTable( {
+        columnDefs: [ {
+            orderable: false,
+            className: 'select-checkbox',
+            targets:   0
+        } ],
+        select: {
+            style:    'os',
+            selector: 'td:first-child'
+        },
+        order: [[ 1, 'asc' ]]
+    } );
+} );*/
 })
 </script>
     
