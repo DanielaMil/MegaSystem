a0e4f33bd1ebfefde9a9045ec63724d695edb654
Daniela <pmil@unprg.edu.pe>
2020-01-10 17:09:40 -0500

    probando3

--- resources/views/Mantenedor/ReporteAlumnosPorCiclo.blade.php
+++ resources/views/Mantenedor/ReporteAlumnosPorCiclo.blade.php
@@ -1,8 +1,18 @@
 @extends('layouts.app')
 
-
 @section('url')
-PATRICIA
+    <script type="text/javascript" src="{{asset('chartjs/dist/Chart.js')}}"></script>
+    <script type="text/javascript" src="{{asset('chartjs/dist/Chart.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('chartjs/dist/utils.js')}}"></script>
+   <script type="text/javascript" src="{{asset('chartjs/dist/Chart.bundle.js')}}"></script>
+    <script type="text/javascript" src="{{asset('chartjs/dist/Chart.bundle.min.js')}}"></script>
+	<style>
+	canvas {
+		-moz-user-select: none;
+		-webkit-user-select: none;
+		-ms-user-select: none;
+	}
+	</style>
 @endsection
 
 @section('content')   
@@ -15,14 +25,56 @@ PATRICIA
             <i class="metismenu-icon pe-7s-graph2"></i>Población de alumnos           
         </div>
 
-        <div class="card-body">
-            <div class="card widget-chart widget-chart2 text-left w-100">
-                <div class="widget-chat-wrapper-outer">
-                    <div class="widget-chart-wrapper widget-chart-wrapper-lg opacity-10 m-0"><div class="chartjs-size-monitor" style="position: absolute; left:. 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;"><div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div></div><div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>
-                        <canvas id="canvas" width="0" height="225" class="chartjs-render-monitor" style="display: block; width: 0px; height: 225px;"></canvas>
-                    </div>
-                </div>
+        
+        <div class="card-body table-responsive no-padding">
+            <div class="col-md-7 ">
+                <canvas id="myChart" width="0" height="400"></canvas>
+                
+            </div>
+            <div class="col-md-5">
             </div>
         </div>
     </div>
+
+    <canvas id="myChart" width="400" height="400"></canvas>
+    <script>
+        var ctx = document.getElementById('myChart');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    </script>
+    
 @endsection
