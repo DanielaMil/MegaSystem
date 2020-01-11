66cfcff1dfd23c0574a2b51d5bbdd5fd99f457d5
Daniela <pmil@unprg.edu.pe>
2020-01-11 13:38:00 -0500

    Probando 5

--- resources/views/Mantenedor/ReporteAlumnosPorCiclo.blade.php
+++ resources/views/Mantenedor/ReporteAlumnosPorCiclo.blade.php
@@ -21,17 +21,32 @@
 
     <div class="card">
         <div class="card-header">
-            <i class="metismenu-icon pe-7s-graph2"></i>Población de alumnos           
+            <i class="metismenu-icon pe-7s-graph3"></i>Población de alumnos           
         </div>
-
-        
+       
         <div class="card-body table-responsive no-padding">
-            <div class="col-md-7 ">
-                <canvas id="myChart" width="0" height="400"></canvas>
-                
+            <div class="row">
+                <div class="col-md-7 ">
+                    <canvas id="myChart" width="0" height="400"></canvas>                   
+                </div>
+                <div class="col-md-5">
+                    <table class="mb-0 table table-bordered table-sm">
+                        <thead>
+                            <tr>
+                                <th class="text-center"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Ciclo</font></font></th>
+                                <th class="text-center"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Matriculados</font></font></th>
+                                <th class="text-center"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Curso</font></font></th>
+                                <th class="text-center"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Grupo</font></font></th>
+                                <th class="text-center"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Retirados</font></font></th>
+                            </tr>
+                        </thead>
+                        <tbody id="tblReporte3">
+                            
+                        </tbody>
+                    </table>
+                </div>
             </div>
-            <div class="col-md-5">
-            </div>
+            
         </div>
     </div>
 
@@ -41,7 +56,7 @@
         var myChart = new Chart(ctx, {
             type: 'bar',
             data: {
-                labels: ['Ciclo 2020-I', 'Ciclo 2019-III', 'Ciclo 2019-II', 'Ciclo 2019-I', 'Ciclo 2018-III', 'Ciclo 2018-II'],
+                labels: ['Ciclo 2020-I', 'Ciclo 2019-III', 'Ciclo 2019-II', 'Ciclo 2019-I', 'Ciclo 2018-III', 'Ciclo 2018-I'],
                 datasets: [{
                     label: '# Matriculados',
                     data: [12, 19, 3, 5, 2, 3],
